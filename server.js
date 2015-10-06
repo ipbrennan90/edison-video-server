1ad69bbd12bc98a53427942402213f9770acfe11
Ian Brennan <ian.brennan@icloud.com>
2015-10-06 13:03:01 -0600

    need to add lib files

---
+++ server.js
@@ -0,0 +1,50 @@
+// modules
+// I changed !
+var ws = require('ws');;
+
+// configuration files
+var configServer = require('./lib/config/server');
+
+// app parameters
+
+// serve index
+
+
+/// Video streaming section
+// Reference: https://github.com/phoboslab/jsmpeg/blob/master/stream-server.js
+
+var STREAM_MAGIC_BYTES = 'jsmp'; // Must be 4 bytes
+var width = 320;
+var height = 240;
+
+// WebSocket server
+var wsServer = new (ws.Server)({ port: configServer.wsPort });
+console.log('WebSocket server listening on port ' + configServer.wsPort);
+
+wsServer.on('connection', function(socket) {
+  // Send magic bytes and video size to the newly connected socket
+  // struct { char magic[4]; unsigned short width, height;}
+  var streamHeader = new Buffer(8);
+
+  streamHeader.write(STREAM_MAGIC_BYTES);
+  streamHeader.writeUInt16BE(width, 4);
+  streamHeader.writeUInt16BE(height, 6);
+  socket.send(streamHeader, { binary: true });
+
+  console.log('New WebSocket Connection (' + wsServer.clients.length + ' total)');
+
+  socket.on('close', function(code, message){
+    console.log('Disconnected WebSocket (' + wsServer.clients.length + ' total)');
+  });
+});
+
+wsServer.broadcast = function(data, opts) {
+  for(var i in this.clients) {
+    if(this.clients[i].readyState == 1) {
+      this.clients[i].send(data, opts);
+    }
+    else {
+      console.log('Error: Client (' + i + ') not connected.');
+    }
+  }
+};
